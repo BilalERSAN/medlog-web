52f179df8a60f7186398263a7084de4fad35f95c
Bilal Bugra Ersan <bilalbugraersan@gmail.com>
2026-05-29 15:21:50 +0300

    robot ts güncelelndi

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -11,6 +11,6 @@ export default function robots(): MetadataRoute.Robots {
         '/api/admin/*',
       ],
     },
-    sitemap: 'https://curelogturkey.com/sitemap.xml',
+    sitemap: 'https://www.curelogturkey.com/sitemap.xml',
   };
 }
